4d7197e5aaf44b8282f85c55e5f7aa0ffb5f7d67
남덕우 <78715592+dukwoo555@users.noreply.github.com>
2021-02-20 15:32:17 +0900

    Add files via upload

---
+++ instawash_pjt/constant_v.py
@@ -0,0 +1,31 @@
+# gurobi license 위치
+license_location = './gurobi.lic'
+
+# DB정보
+host = 
+user = 
+password = 
+db = 
+
+# naver API
+naverID = 
+naverPW = 
+
+# 근무시작시간
+# 예) 9시 30분: 9.5, 10시: 10
+workStartTime = 9.5
+
+# 최소시작시간(분)
+minEndTime = 60
+
+# 근무 시간
+workingTime = 600
+w_start_time = 10 * 60
+overTimeMem = 3
+overTime = 660
+
+# penalty constant value M
+const_M = 8100
+
+# penalty weight
+penalty = 0.01
